f410ac83c3ed092db7c5fe91c08a3653d5429311
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2026-05-12 08:24:06 +0200

    Added project 9

---
+++ 9-ethers/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id="center"></section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  );
+}
+
+export default App;
